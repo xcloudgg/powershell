1e04b93a84784990d5e95a42eebcd212bbfaac4a
xc0-marius <mariuskris@pm.me>
2026-09-17 13:25:57 +0200

    Create 02-ad.ps1 for AD DS installation and promotion
    This script installs Active Directory Domain Services (AD DS) and promotes the server to a domain controller with DNS.

---
+++ 02-ad.ps1
@@ -0,0 +1,23 @@
+# 02-ADDS.ps1 - Installer AD DS + DNS og promoter til domenekontroller (ny skog)
+$DomeneNavn  = "ad.eikerikt.com"
+$NetBIOS     = "eIKT"
+$DSRMPassord = Read-Host "Hokksund" -AsSecureString
+
+try {
+    if (-not (Get-WindowsFeature AD-Domain-Services).Installed) {
+        Install-WindowsFeature -Name AD-Domain-Services,DNS `
+            -IncludeManagementTools -ErrorAction Stop
+    }
+    Import-Module ADDSDeployment
+    Install-ADDSForest `
+        -DomainName $DomeneNavn `
+        -DomainNetbiosName $NetBIOS `
+        -InstallDns `
+        -SafeModeAdministratorPassword $DSRMPassord `
+        -ForestMode "WinThreshold" `
+        -DomainMode "WinThreshold" `
+        -Force
+    # Serveren restarter automatisk etter vellykket promotering.
+} catch {
+    Write-Error "Feil ved ADDS-promotering: $_"
+}
